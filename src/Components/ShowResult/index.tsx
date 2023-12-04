550d293b89970eb17b94fb59dfc0de61239264c6
Miguel Parra <shragath@gmail.com>
2023-12-03 23:51:12 -0600

    Componente que muestra el resultado del n-esimo numero de la serie

--- src/Components/ShowResult/index.tsx
+++ src/Components/ShowResult/index.tsx
@@ -1,16 +1,37 @@
-
-const ShowRes = ({ value = 0 }: { value: number }) => {
+const ShowRes = ({
+    valueSerie = 0,
+    valueFibonacci = 0,
+    valueTriangular = 0,
+    valuePrimo = 0,
+    nValue = 0,
+}: {
+    valueSerie: number;
+    valueFibonacci: number;
+    valueTriangular: number;
+    valuePrimo: number;
+    nValue: number;
+}) => {
     return (
         <>
             <div>
                 <label>
-                    Equation: serie(n) = 2 fibonacci(n - 2) + 3 triangular(n -
-                    2) - 7 primo(n - 1)
+                    Equation: serie({nValue <= 0 ? 'n' : nValue}) = 2 fibonacci(
+                    {nValue <= 0 ? 'n' : nValue} - 2) + 3 triangular(
+                    {nValue <= 0 ? 'n' : nValue} - 2) - 7 primo(
+                    {nValue <= 0 ? 'n' : nValue} - 1)
                 </label>
             </div>
             <div>
+                <p>
+                    Fibonacci({nValue - 2}) = {valueFibonacci}, Triangular(
+                    {nValue - 2}) = {valueTriangular}, Primo({nValue - 1}) ={' '}
+                    {valuePrimo}
+                </p>
+            </div>
+            <div>
                 <label>
-                    Result: <b>{value}</b>
+                    Resultado = {2 * valueFibonacci} + {3 * valueTriangular} -{' '}
+                    {7 * valuePrimo} = <b>{valueSerie}</b>
                 </label>
             </div>
         </>
