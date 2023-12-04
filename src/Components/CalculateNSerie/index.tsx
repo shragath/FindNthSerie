e364761db424c0849168f58d15a62fbd806d2300
Miguel Parra <shragath@gmail.com>
2023-12-03 23:50:52 -0600

    Componente que realiza el calculo de la serie

--- src/Components/CalculateNSerie/index.tsx
+++ src/Components/CalculateNSerie/index.tsx
@@ -2,8 +2,9 @@ import ShowRes from '../ShowResult';
 import Equations from './types';
 
 function CalculateNSerie({ n = 0 }: { n: number }) {
-    const serie = new Equations(n).findNthFibonacci(n);
-    return <ShowRes value={serie} />;
+    const equation = new Equations(n);
+
+    return <ShowRes valueSerie={equation.findNthSerie()} valueFibonacci={equation.resFibonacci} valueTriangular={equation.resTriangular} valuePrimo={equation.resPrimo} nValue={n} />;
 }
 
 export default CalculateNSerie;
