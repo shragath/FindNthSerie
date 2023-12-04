4d79218e3409776bf3594fc69f8157638e3398ba
Miguel Parra <shragath@gmail.com>
2023-12-03 23:49:24 -0600

    Tests de la clase para resolver la ecuación de la n-esimo numero de la serie

--- src/Components/CalculateNSerie/Equation.test.tsx
+++ src/Components/CalculateNSerie/Equation.test.tsx
@@ -1,32 +1,44 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 // import { render } from "@testing-library/react"
-import Equations from './types'
+import Equations from './types';
 
 describe('Test Equations', () => {
-    it('Test n Fibonacci serie, should return 55', () => {
-        const n = 10
-        const expect_res = 55
+    it('Test n Fibonacci serie, should return [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55].', () => {
+        const n = Array.from(Array(11).keys());
+        const fibonacci = n.map((i) => {
+            return new Equations(i).findNthFibonacci(i);
+        });
 
-        const Fibonacci = new Equations(n).findNthFibonacci(n)
+        expect(fibonacci).toEqual<number[]>([0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]);
+    });
 
-        expect(Fibonacci).toEqual<number>(expect_res)
-    })
+    it('Test n Triangular Serie, should return [0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55]', () => {
+        const n = Array.from(Array(11).keys());
+        const triangular = n.map((i) => {
+            return new Equations(i).findNthTriangular(i);
+        });
 
-    it('Test n Triangular Serie, should return 21', () => {
-        const n = 6
-        const expect_res = 21
+        expect(triangular).toEqual<number[]>([
+            0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55
+        ]);
+    });
 
-        const triangular = new Equations(n).findNthTriangular(n)
+    it('Test n Prime number, should return [0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 55, 29]', () => {
+        const n = Array.from(Array(11).keys());
+        const primo = n.map((i) => {
+            return new Equations(i).findNthPrimo(i);
+        });
 
-        expect(triangular).toEqual<number>(expect_res)
-    })
+        expect(primo).toEqual<number[]>([0, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
+    });
 
-    it('Test n Prime number, should return 31', () => {
-        const n = 11
-        const expect_res = 31
+    it('Test n serie number', () => {
+        const n = Array.from(Array(10).keys());
 
-        const prime = new Equations(n).findNthPrimo(n)
+        const serie = n.map((i) => {
+            return new Equations(i).findNthSerie();
+        });
 
-        expect(prime).toEqual<number>(expect_res)
-    })
-})
+        expect(Array.isArray(serie)).toBe(true);
+    });
+});
