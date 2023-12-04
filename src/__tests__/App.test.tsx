36d87f786d198befeef99cfc696e5ad7f666aced
Miguel Parra <shragath@gmail.com>
2023-12-03 23:48:46 -0600

    Test de renderizado de los componentes

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,9 +1,16 @@
-import '@testing-library/jest-dom'
-import { render } from '@testing-library/react'
-import App from '../App'
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import App from '../App';
+import renderer from 'react-test-renderer';
 
 // Test renders App components
-test('Renders the main page', () => {
-    render(<App />)
-    expect(true).toBeTruthy()
-})
+describe('Renders components sin fallos', () => {
+    it('Renders App', () => {
+        render(<App />);
+    });
+
+    it('Prueba de comparacion snapshot', () => {
+        const tree = renderer.create(<App />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
