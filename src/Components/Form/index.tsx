4db795962f7655bd9cad5c1d52a85b9ab8cec9b5
Miguel Parra <shragath@gmail.com>
2023-12-03 23:51:41 -0600

    Componente que muestra formulario

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -1,18 +1,10 @@
 import { useState } from 'react';
 import CalculateNSerie from '../CalculateNSerie';
-import Equations from '../CalculateNSerie/types';
+import EqFormElement from './types';
 
-interface FormElements extends HTMLFormControlsCollection {
-    nInput: HTMLInputElement;
-}
-
-interface EqFormElement extends HTMLFormElement {
-    readonly elements: FormElements;
-}
 
 const ShowEqForm = () => {
     const [n, setN] = useState(0);
-    const [nFibonacci, setNFibonacci] = useState(0);
 
     const handleSubmit = (e: React.FormEvent<EqFormElement>) => {
         e.preventDefault();
@@ -21,10 +13,7 @@ const ShowEqForm = () => {
 
         try {
             const nInput = Number.parseInt(form.elements.nInput.value);
-
             setN(nInput);
-            const test: Equations = new Equations(nInput);
-            setNFibonacci(test.findNthFibonacci(nInput));
         } catch (error) {
             throw new Error('El valor no es un numero');
         }
@@ -34,19 +23,20 @@ const ShowEqForm = () => {
         <>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="nInput">
-                    Escribe el numero (n) de la serie ({n}) a calcular:
+                    Escribe el numero (n) de la serie:
                 </label>
 
                 <input
                     id="nInput"
                     type="number"
                     title="n series value"
+                    placeholder="Number"
                     required
                 />
-                <button type="submit">Calcular</button>
+                <button type="submit" id="buttonCalcular">Calcular</button>
             </form>
 
-            <CalculateNSerie value={n} />
+            <CalculateNSerie n={n} />
         </>
     );
 };
